08987816b3ba0c4f4c38cd8e595453620fe3a204
cwlui-del <cwlui@vtc.edu.hk>
2026-08-19 15:15:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ st.markdown("---")
 st.markdown("### 🗺️ 校園樓層分佈圖 (General Map)")
 with st.expander("🏢 點擊展開查看校園樓層指南"):
     st.markdown("""
-        <img src='https://github.com/cwlui-del/hls-orientation/blob/main/Directory.png?raw=true' class='map-box' alt='校園指南'>
+        <img src='https://github.com/cwlui-del/hls-orientation/blob/main/Directory.png?raw=true' class='map-box' alt='校園指南G/F'>
     """, unsafe_allow_html=True)
 
 st.markdown("### ⚠️ 緊急通知與聯絡資訊")
